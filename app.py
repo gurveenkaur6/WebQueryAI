7d8c9e3678845893e7509f06e2fef0872e63a319
Gurveen <gurveenkaursahni@gmail.com>
2024-07-29 16:26:29 -0400

    first commit

--- app.py
+++ app.py
@@ -2,33 +2,52 @@ from langchain_openai import ChatOpenAI
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.output_parsers import StrOutputParser
 
+
 import streamlit as st
 import os
 from dotenv import load_dotenv
 
 os.environ["OPENAI_API_KEY"]=os.getenv("OPENAI_API_KEY")
-## Langsmith tracking
+## Enables the tracing for Langsmith globally.
+## Langchain automatically traces all interactions without needing to decorate individual functions
 os.environ["LANGCHAIN_TRACING_V2"]="true"
+
+## API key for Langchain to use the Langsmith service
 os.environ["LANGCHAIN_API_KEY"]=os.getenv("LANGCHAIN_API_KEY")
 
+
 # Prompt Template
-prompt=ChatPromptTemplate.from_messages(
+prompt = ChatPromptTemplate.from_messages(
     [
-        ("system","You are a helpful assistant. Please respond to the user queries"),
-        ("user","Question:{question}")
+        ("system", "You are an expert travel assistant. Please help the user plan their trip."),
+        ("user", "Travel Preferences based on Destination in a table UI: {preferences}, {destination}")
     ]
 )
 
 # streamlit framework
+st.title('AI Travel Planner') ## title
+st.write('Plan your trip with personalized itineraries and local recommendations!')
 
-st.title('Langchain Demo With OPENAI API')
-input_text=st.text_input("Search the topic u want")
+# Input fields for travel preferences
+destination = st.text_input("Enter your travel destination")
+preferences = st.text_area("Enter your travel preferences (e.g., interests, budget, duration)")
 
-# integrate the chat prompt with the llm and setup an output parser to get the string response form the model
+
+## instance of the ChatOpenAI model
 llm=ChatOpenAI(model="gpt-3.5-turbo")
+
+## an output parser setup to get string responses
 output_parser=StrOutputParser()
 chain=prompt|llm|output_parser
 
 # give the input as a question/prompt to the llm model
-if input_text:
-    st.write(chain.invoke({'question':input_text}))
+##  If the user provides input, the chain is invoked with the input and the response is displayed.
+if st.button("Generate Itinerary"):
+    if destination and preferences:
+        response= chain.invoke({'preferences': preferences, 'destination': destination})
+        st.write(response)
+    else:
+        st.write("Please enter both destination and preferences.")
+
+
+
